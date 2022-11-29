d6c3613e308e8f2ccd71f1ee281ec95ac7cb63df
sky9023 <25349023.qq@gmail.com>
2022-11-30 00:01:29 +0800

    [UPDATE] migrate to .py: ransac matching; refactor the code

--- 1/HW3-1.py
+++ 1/HW3-1.py
@@ -4,8 +4,6 @@ from collections import namedtuple
 from itertools import chain
 
 import cv2
-import matplotlib
-import matplotlib.pyplot as plt
 import numpy as np
 
 SiftFeature = namedtuple('SiftFeature', 'keypoints desc')
@@ -15,7 +13,6 @@ def sift_detect(image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     sift = cv2.SIFT_create(0, 7)
     kp, descriptor = sift.detectAndCompute(gray, None)
-    print(len(kp))
     return SiftFeature(kp, descriptor)
 
 
@@ -30,7 +27,7 @@ def sift_matching(left_desc, right_desc, threshold=200):
     return matches
 
 
-def coords_from_matches(matches, left_feat: SiftFeature, right_feat: SiftFeature):
+def points_from_matches(matches, left_feat: SiftFeature, right_feat: SiftFeature):
     return [[left_feat.keypoints[m.queryIdx].pt, right_feat.keypoints[m.trainIdx].pt]
             for m in matches]
 
@@ -51,9 +48,50 @@ def solve_homography(pts1, pts2):
     return x
 
 
-if __name__ == '__main__':
-    matplotlib.use('agg')
+def ransac_matching(points, rounds=1000, agree_threshold=100):
+    max_agrees = -1
+    best_agree_idx = None
+    for _ in range(rounds):
+        rand_quad = random_select_quad(points)
+        H = solve_homography(*rand_quad)
 
+        left_pts, right_pts = separate_left_right_points(points)
+        warpped_left_pts = warp_by(left_pts, H)
+
+        distance = np.linalg.norm(warpped_left_pts - right_pts, axis=1)
+        agrees = distance < agree_threshold
+        if agrees.sum() > max_agrees:
+            max_agrees = agrees.sum()
+            best_agree_idx = agrees
+
+    return best_agree_idx
+
+
+def random_select_quad(coords):
+    rand_coords = random.choices(coords, k=4)
+    rand_coords = list(zip(*rand_coords))
+    return rand_coords
+
+
+def warp_by(pts, H):
+    warpped = (H @ pts[..., np.newaxis]).reshape((-1, 3))
+    warpped /= warpped[:, 2:]
+    return warpped
+
+
+def separate_left_right_points(points):
+    points = np.array(list(zip(*points)))
+    book_pts, image_pts = np.concatenate([points, np.ones_like(points)[..., :1]], axis=-1)
+    return book_pts, image_pts
+
+
+def pick_agreed_keypoints(feat, matches, idx_name, agreements):
+    matched_kps = [feat.keypoints[getattr(m, idx_name)] for m in matches]
+    agreed_kps = [mkp for (agree, mkp) in zip(agreements, matched_kps) if agree]
+    return agreed_kps
+
+
+if __name__ == '__main__':
     output_dir = pathlib.Path('output')
     if not output_dir.exists():
         output_dir.mkdir(parents=True, exist_ok=True)
@@ -64,12 +102,28 @@ if __name__ == '__main__':
     image_feat = sift_detect(image)
 
     for i, (book, thres) in enumerate(zip(books, thresholds)):
+        # 1-A
         book_feat = sift_detect(book)
-
         matches_idx = sift_matching(book_feat.desc, image_feat.desc, thres)
+
         matches_img = cv2.drawMatchesKnn(
             book, book_feat.keypoints, image, image_feat.keypoints, matches_idx, None,
             flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS,
             matchColor=(0, 255, 0))
         cv2.imwrite(str(output_dir / f'1a_book{i + 1}.jpg'), matches_img)
 
+        # 1-B
+        flattened_matches = list(chain.from_iterable(matches_idx))
+        pts = points_from_matches(flattened_matches, book_feat, image_feat)
+
+        best_agrees = ransac_matching(pts, agree_threshold=50)
+
+        matches_idx = [[cv2.DMatch(i, i, 0)] for i in range(best_agrees.sum())]
+        matched_book_kp = pick_agreed_keypoints(book_feat, flattened_matches, 'queryIdx', best_agrees)
+        matched_image_kp = pick_agreed_keypoints(image_feat, flattened_matches, 'trainIdx', best_agrees)
+
+        matches_img = cv2.drawMatchesKnn(
+            book, matched_book_kp, image, matched_image_kp, matches_idx, None,
+            flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS,
+            matchColor=(0, 255, 0))
+        cv2.imwrite(str(output_dir / f'1b_book{i + 1}.jpg'), matches_img)
